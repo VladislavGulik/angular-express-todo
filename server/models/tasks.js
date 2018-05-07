cfd051013b4b5bdbf2991c6c3599271c48f75d08
Vladislav Gulik <hulik.uladzislau@gmail.com>
2018-05-07 13:14:09 +0300

    Start bundle for app implemented

---
+++ server/models/tasks.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const tasksSchema = new mongoose.Schema({
+	task: { type: String }, 
+},
+{ collection: "tasks", strict: false });
+
+module.exports = mongoose.model('Tasks', tasksSchema);
+
+module.exports = {
+
+	saveTasks: function (result) {
+		const newTask = new Result(result.body);
+		return newTask.save((err) => {
+			if (err) {
+				console.log(err);
+			}
+		});
+	},
+
+	getTasks: function (task) {
+		return Tasks.find({}, function (err, result) {
+			return result;
+		});
+	}
+}
+
